535607645da3a1ce6e7aec81eadb379a8f4ce447
Carson Day <154829418+CarsonDay11@users.noreply.github.com>
2023-12-29 11:38:44 -0600

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,69 +1,56 @@
 import sys
+import os
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
+os.environ['QTWEBENGINE_CHROMIUM_FLAGS'] = '--disable-web-security'
 
 class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://carsonday.pro"))
+        self.browser.setUrl(QUrl("https://car-browser.vercel.app/"))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        # Navigation Bar
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        # Back Button
         back_btn = QAction('Back', self)
-        back_btn.setStatusTip('Back to the previous page')
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        # Forward Button
         forward_btn = QAction('Forward', self)
-        forward_btn.setStatusTip('Forward to the next page')
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        # Reload Button
         reload_btn = QAction('Reload', self)
-        reload_btn.setStatusTip('Reload the page')
         reload_btn.triggered.connect(self.browser.reload)
         navbar.addAction(reload_btn)
 
-        # Home Button
         home_btn = QAction('Home', self)
-        home_btn.setStatusTip('Go home')
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-        # URL Bar
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
 
-        # Updating URL bar
         self.browser.urlChanged.connect(self.update_urlbar)
 
-        # Theme Combo Box
         self.theme_combobox = QComboBox()
         self.theme_combobox.addItems(["Light Theme", "Dark Theme"])
         self.theme_combobox.setCurrentText("Light Theme")
         self.theme_combobox.currentIndexChanged.connect(self.apply_theme)
         navbar.addWidget(self.theme_combobox)
 
-        # Apply the initial theme
         self.apply_theme()
 
-        # Settings Action
         settings_action = QAction('Settings', self)
-        settings_action.setStatusTip('Open Settings')
         settings_action.triggered.connect(self.open_settings)
         navbar.addAction(settings_action)
 
-        # Default settings
         self.settings = QSettings("MyCompany", "MyBrowser")
         self.home_url = self.settings.value("home_url", "http://www.google.com")
 
@@ -97,22 +84,18 @@ class SettingsDialog(QDialog):
         super(SettingsDialog, self).__init__(parent)
         self.setWindowTitle('Browser Settings')
 
-        # Home page URL
         self.home_label = QLabel('Home Page URL:')
         self.home_edit = QLineEdit(self.parent().home_url)
         self.home_edit.setPlaceholderText('Enter the home page URL')
 
-        # Theme Combo Box
         self.theme_label = QLabel('Select Theme:')
         self.theme_combobox = QComboBox()
         self.theme_combobox.addItems(["Light Theme", "Dark Theme"])
         self.theme_combobox.setCurrentText("Light Theme")
 
-        # Save button
         self.save_button = QPushButton('Save')
         self.save_button.clicked.connect(self.save_settings)
 
-        # Layout
         layout = QVBoxLayout()
         layout.addWidget(self.home_label)
         layout.addWidget(self.home_edit)
@@ -127,10 +110,9 @@ class SettingsDialog(QDialog):
         self.parent().home_url = home_url
         self.parent().settings.setValue("home_url", home_url)
 
-        # Save Theme
         selected_theme = self.theme_combobox.currentText()
         self.parent().settings.setValue("theme", selected_theme)
-        self.parent().apply_theme()  # Apply the theme immediately
+        self.parent().apply_theme()
 
         self.accept()
 
